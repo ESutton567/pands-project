02cb7a95b040321edd143f3bbf10610500558dbc
Eilis Sutton <G00411335@gmit.ie>
2022-04-05 22:32:34 +0100

    Add: explanation of different analyses in the program

--- analysis.py
+++ analysis.py
@@ -1,1 +1,24 @@
-# This program performs some basic analysis on the Fisher's Iris data set
+# This program performs some basic data analysis on the Fisher's Iris data set
+# Author: Éilis Sutton
+
+# The below code outputs a summary of each variable to a single text file
+
+
+
+
+
+
+
+
+# The below code saves a histogram of each variable to png files
+
+
+
+
+
+# The below code outputs a scatter plot of each pair of variables
+
+
+
+
+# The below code ......
