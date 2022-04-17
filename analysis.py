33bc262127db737e871073e857dfc4c5eea9a462
Eilis Sutton <G00411335@gmit.ie>
2022-04-17 11:57:19 +0100

    Add: assign column names

--- analysis.py
+++ analysis.py
@@ -8,27 +8,28 @@ filename = path + 'iris.data'
 
 df = pd.read_csv(filename)
 
+header_list=['Sepal Length','Sepal Width','Petal Length','Petal Width','Species']
+df = pd.read_csv(filename, names=header_list)
+
 print(df.head(5))
 
 
 # The below code outputs a summary of each variable to a single text file
 
+# open iris dataset in read only mode
+#f = open("iris.data", "r")
+# create new txt file
+#f.open("irisVariablesSummary.txt", "x")
+# write to the new txt file
+#f.write("irisVariablesSummary.txt")
 
-
-
-
-
+#print(f.read())
 
 
 # The below code saves a histogram of each variable to png files
 
-
-
-
-
 # The below code outputs a scatter plot of each pair of variables
 
-
-
-
 # The below code ......
+
+# The below code prints out the average of each variable for 
