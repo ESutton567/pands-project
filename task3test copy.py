9c07af4506a263e91084ace8d8ffebd916516330
Eilis Sutton <G00411335@gmit.ie>
2022-04-26 22:39:07 +0100

    Add: task 3: code for customising scatter plots in test program

--- task3test copy.py
+++ task3test copy.py
@@ -1,6 +1,7 @@
 # Task 3: The below code outputs a scatter plot of each pair of variables
 # Author: Éilis Sutton
 
+from matplotlib.ft2font import BOLD
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as py
@@ -14,33 +15,42 @@ iris_data.columns = ['sepal_length','sepal_width','petal_length','petal_width','
 #versicolor = iris_data[iris_data.columns 'species' == 'Versicolor']
 
 # generating a scatter plot matrix to look at the interaction between the variables
-fig,ax = plt.subplots(6, figsize=(18,15))
+fig,ax = plt.subplots(6, figsize=(20,20))
+    
+ax[0].scatter(x = iris_data['sepal_length'], y = iris_data['sepal_width'], color = 'blue', edgecolors = "black")
+ax[0].set_xlabel("Sepal Length", weight='bold')
+ax[0].set_ylabel("Sepal Width", weight='bold')
+ax[0].set_title('Iris dataset: Scatter plots of each pair of variables', 
+                fontname='Times New Roman',
+                fontsize=35, 
+                fontweight='bold'
+                )
 
-ax[0].scatter(x = iris_data['sepal_length'], y = iris_data['sepal_width'], color = 'blue')
-ax[0].set_xlabel("Sepal Length")
-ax[0].set_ylabel("Sepal Width")
 
-ax[1].scatter(x = iris_data['sepal_width'], y = iris_data['petal_length'])
-ax[1].set_xlabel("Sepal Width")
-ax[1].set_ylabel("Petal Length")
+ax[1].scatter(x = iris_data['sepal_width'], y = iris_data['petal_length'], color = 'blueviolet', edgecolors = "black")
+ax[1].set_xlabel("Sepal Width", weight='bold')
+ax[1].set_ylabel("Petal Length", weight='bold')
 
-ax[2].scatter(x = iris_data['petal_length'], y = iris_data['petal_width'])
-ax[2].set_xlabel("Petal Length")
-ax[2].set_ylabel("Petal Width")
+ax[2].scatter(x = iris_data['petal_length'], y = iris_data['petal_width'], color = 'darkgreen', edgecolors = "black")
+ax[2].set_xlabel("Petal Length", weight='bold')
+ax[2].set_ylabel("Petal Width", weight='bold')
 
-ax[3].scatter(x = iris_data['sepal_length'], y = iris_data['sepal_width'])
-ax[3].set_xlabel("Sepal Length")
-ax[3].set_ylabel("Sepal Width")
+ax[3].scatter(x = iris_data['sepal_length'], y = iris_data['sepal_width'], color = 'teal', edgecolors = "black")
+ax[3].set_xlabel("Sepal Length", weight='bold')
+ax[3].set_ylabel("Sepal Width", weight='bold')
 
-ax[4].scatter(x = iris_data['petal_width'], y = iris_data['species'])
-ax[4].set_xlabel("Petal Width")
-ax[4].set_ylabel("Species")
+ax[4].scatter(x = iris_data['petal_width'], y = iris_data['species'], color = 'slateblue', edgecolors = "black")
+ax[4].set_xlabel("Petal Width", weight='bold')
+ax[4].set_ylabel("Species", weight='bold')
 
-ax[5].scatter(x = iris_data['species'], y = iris_data['sepal_length'])
-ax[5].set_xlabel("Species")
-ax[5].set_ylabel("Sepal Length")
+ax[5].scatter(x = iris_data['species'], y = iris_data['sepal_length'], color = 'orchid', edgecolors = "black")
+ax[5].set_xlabel("Species", weight='bold')
+ax[5].set_ylabel("Sepal Length", weight='bold')
+
 
 plt.grid()
 plt.show()
 
+# the code below was used to visualise the scatterplots while customising
+#plt.savefig("scatterplot.png")
 
