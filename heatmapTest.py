83fcc00e3e885117bb9f5fdc602947e05733a0f8
Eilis Sutton <G00411335@gmit.ie>
2022-05-03 22:35:39 +0100

    Add: working Heatmap code in test program file

---
+++ heatmapTest.py
@@ -0,0 +1,26 @@
+# this program runs a heatmap of the iris dataset
+# Author: Éilis Sutton
+# Ref: https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
+
+import pandas as pd
+import csv
+import re
+import matplotlib.pyplot as plt
+# create the plot using seaborn
+import seaborn as sns
+
+path = '../pands-project/'
+filename = path + 'iris_data.csv'
+# assign column names
+columns = ['sepal_length','sepal_width','petal_length','petal_width', 'species']
+
+#read in file -> need to reread in the file to use seaborn
+iris_data = pd.read_csv(filename, sep= ',', header=None, names=columns)
+
+sns.heatmap(iris_data.corr(), linecolor = 'white', linewidths = 1, annot = True)
+
+plt.savefig("iris_data_Heatmap.png")
+
+
+
+
